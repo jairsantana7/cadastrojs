9df452501ce6a8a47c0c924c3f9dc2c7ebb43eb0
unknown <jairsantana7@gmail.com>
2021-10-13 23:28:05 -0300

    NegociacaoControllers

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,21 +5,40 @@ class NegociacaoController {
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
     //this._campos = [inputData, inputQuantidade, inputValor];
-  }
-
-  get inputData() {
-    return this.inputData;
+    this._listaNegociacoes = new ListaNegociacoes();
   }
 
   adiciona(event) {
     event.preventDefault();
-    //let data = new Date(this._inputData.value.split("-"));
-    let data = new Date(this._inputData.value.replace(/-/g, ",")); //com expressão regular
-    let negociacao = new Negociacao(
-      data,
-      this._inputQuantidade,
-      this._inputValor
+    //let negociacao = _criaNegociacao();
+    // let negociacao = new Negociacao(
+    //   DateHelper.textoParaData(this._inputData.value),
+    //   this._inputQuantidade.value,
+    //   this._inputValor.value
+    // );
+
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    this._limpaFormulario();
+    console.log(this._listaNegociacoes.mostraNegociacoes);
+
+    //let tste = [1, 2, 3, 4, 5, 6];
+    //this._listaNegociacoes.adiciona(tste);
+
+    //console.log(this.ListaNegociacoes.negociacoes());
+  }
+
+  _criaNegociacao() {
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
+      this._inputQuantidade.value,
+      this._inputValor.value
     );
-    console.log(negociacao);
+  }
+
+  _limpaFormulario() {
+    this._inputData.value = "";
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
+    this._inputData.focus();
   }
 }
